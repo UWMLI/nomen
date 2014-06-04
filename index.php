9f822aa0379a621b9ea108947300a84710573267
Michael Tolly <miketolly@gmail.com>
2014-06-04 16:29:32 -0500

    image rows made dynamically in js

--- index.php
+++ index.php
@@ -12,27 +12,23 @@
         clear: left;
         white-space: nowrap;
         overflow-x: hidden;
-        background-color: #333;
+        background-color: #AAA;
+        padding-top: 6px;
         padding-left: 6px;
       }
-      .row:first-child {
-        padding-top: 5px;
-      }
       .row:last-child {
-        padding-bottom: 1px;
+        padding-bottom: 6px;
       }
       .row-button {
         position: relative;
         display: inline;
       }
-      .row-button:last-child {
-        padding-right: 6px;
-      }
       .row-image {
         left: 0;
         top: 0;
         height: 200px;
-        margin: 2px;
+        margin-right: 6px;
+        border: 1px solid #444;
       }
       .row-text {
         position: absolute;
@@ -46,8 +42,36 @@
       }
     </style>
     <script type="text/javascript">
+      var images =
+        { 'Owl': 'http://upload.wikimedia.org/wikipedia/commons/f/f1/African_Scops_owl.jpg'
+        , 'Parrot': 'http://upload.wikimedia.org/wikipedia/commons/8/81/Red_Lory_%28Eos_bornea%29-6.jpg'
+        , 'Flycatcher': 'http://upload.wikimedia.org/wikipedia/commons/5/5d/Restless_flycatcher04.jpg'
+        , 'Sunbittern': 'http://upload.wikimedia.org/wikipedia/commons/6/69/Stavenn_Eurypiga_helias_00.jpg'
+        , 'Flamingo': 'http://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Caribbean_Flamingo2_%28Phoenicopterus_ruber%29_%280424%29_-_Relic38.jpg/760px-Caribbean_Flamingo2_%28Phoenicopterus_ruber%29_%280424%29_-_Relic38.jpg'
+        };
+
+      var makeRow = function(ids){
+        var row = $('<div />', {'class': 'row'});
+        ids.forEach(function(id){
+          var button = $('<span />', {'class': 'row-button'});
+          var img = $('<img />', {'class': 'row-image', 'src': images[id]});
+          var text = $('<div class="row-text">'+id+'</div>');
+          button.append(img);
+          button.append(text);
+          row.append(button);
+        });
+        row.dragscrollable({dragSelector:'*'});
+        $('.rows').append(row);
+      };
+
+      var deleteRow = function(){
+        $('.row:last-child').remove();
+      };
+
       $(document).ready(function(){
-        $('.row').dragscrollable({dragSelector:'*'});
+        makeRow(['Owl', 'Parrot', 'Flycatcher', 'Sunbittern', 'Flamingo']);
+        makeRow(['Flamingo', 'Sunbittern', 'Owl', 'Parrot', 'Flycatcher']);
+
         // todo: drag with the space between images
       });
     </script>
@@ -61,52 +85,6 @@
     </p>
 
     <div class="rows">
-
-      <div class="row">
-        <span class="row-button">
-          <img class="row-image" src="http://upload.wikimedia.org/wikipedia/commons/f/f1/African_Scops_owl.jpg" />
-          <div class="row-text">Owl</div>
-        </span>
-        <span class="row-button">
-          <img class="row-image" src="http://upload.wikimedia.org/wikipedia/commons/8/81/Red_Lory_%28Eos_bornea%29-6.jpg" />
-          <div class="row-text">Parrot</div>
-        </span>
-        <span class="row-button">
-          <img class="row-image" src="http://upload.wikimedia.org/wikipedia/commons/5/5d/Restless_flycatcher04.jpg" />
-          <div class="row-text">Flycatcher</div>
-        </span>
-        <span class="row-button">
-          <img class="row-image" src="http://upload.wikimedia.org/wikipedia/commons/6/69/Stavenn_Eurypiga_helias_00.jpg" />
-          <div class="row-text">Sunbittern</div>
-        </span>
-        <span class="row-button">
-          <img class="row-image" src="http://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Caribbean_Flamingo2_%28Phoenicopterus_ruber%29_%280424%29_-_Relic38.jpg/760px-Caribbean_Flamingo2_%28Phoenicopterus_ruber%29_%280424%29_-_Relic38.jpg" />
-          <div class="row-text">Flamingo</div>
-        </span>
-      </div>
-
-      <div class="row">
-        <span class="row-button">
-          <img class="row-image" src="http://upload.wikimedia.org/wikipedia/commons/5/5d/Restless_flycatcher04.jpg" />
-          <div class="row-text">Flycatcher</div>
-        </span>
-        <span class="row-button">
-          <img class="row-image" src="http://upload.wikimedia.org/wikipedia/commons/6/69/Stavenn_Eurypiga_helias_00.jpg" />
-          <div class="row-text">Sunbittern</div>
-        </span>
-        <span class="row-button">
-          <img class="row-image" src="http://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Caribbean_Flamingo2_%28Phoenicopterus_ruber%29_%280424%29_-_Relic38.jpg/760px-Caribbean_Flamingo2_%28Phoenicopterus_ruber%29_%280424%29_-_Relic38.jpg" />
-          <div class="row-text">Flamingo</div>
-        </span>
-        <span class="row-button">
-          <img class="row-image" src="http://upload.wikimedia.org/wikipedia/commons/f/f1/African_Scops_owl.jpg" />
-          <div class="row-text">Owl</div>
-        </span>
-        <span class="row-button">
-          <img class="row-image" src="http://upload.wikimedia.org/wikipedia/commons/8/81/Red_Lory_%28Eos_bornea%29-6.jpg" />
-          <div class="row-text">Parrot</div>
-        </span>
-      </div>
 
     </div>
 
