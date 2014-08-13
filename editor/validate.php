7bfa83dd3f6507fd7c3fb40311b785e0576d5b77
Michael Tolly <miketolly@gmail.com>
2014-08-13 16:47:42 -0500

    First validation: check all features have images

--- editor/validate.php
+++ editor/validate.php
@@ -12,22 +12,59 @@ function scandir_real($dir) {
   return $entries;
 }
 
+function canonical($str) {
+  return str_replace( ' ', '_', strtolower($str) );
+}
+
 function validateDataset($dir) {
+  $errors = [];
+
   $info = json_decode( file_get_contents("$dir/info.json") );
   $species = new parseCSV("$dir/species.csv");
-  var_dump($info->title);
-  var_dump($species->data[0]['name']);
-  var_dump($species->titles[0]);
+  // var_dump($info->title);
+  // var_dump($species->data[0]['name']);
+  // var_dump($species->titles[0]);
 
   $species_images = scandir_real("$dir/species");
-  var_dump($species_images[0]);
+  // var_dump($species_images[0]);
 
   $feature_dirs = scandir_real("$dir/features");
   $features = [];
   foreach ($feature_dirs as $feature) {
-    $features[$feature] = scandir_real("$dir/features/$feature");
+    $features[$feature] = [];
+    foreach (scandir_real("$dir/features/$feature") as $image) {
+      $features[$feature][$image] = true;
+    }
   }
-  var_dump($features['stem_texture'][0]);
+  // var_dump($features['stem_texture'][0]);
+
+  // Check that all features have images
+  foreach ($species->data as $spec) {
+    foreach ($spec as $k => $vstr) {
+      $k = canonical($k);
+      if ($k == 'name' || $k == 'display_name' || $k == 'description') continue;
+      $values = array_map( 'trim', explode(',', $vstr) );
+      if ( array_key_exists($k, $features) ) {
+        foreach ($values as $value) {
+          $value = canonical($value);
+          if ( empty($value) ) continue;
+          if ( array_key_exists("$value.png", $features[$k])
+            || array_key_exists("$value.jpg", $features[$k]) ) {
+            // No problem here
+          }
+          else
+          {
+            $errors["Feature value doesn't have an image: $k -> $value"] = true;
+          }
+        }
+      }
+      else {
+        $errors["Feature doesn't have a folder: $k"] = true;
+      }
+    }
+  }
+
+  var_dump($errors);
 }
 
 validateDataset(realpath(dirname(__FILE__)) . '/../fieldguide/plants');
