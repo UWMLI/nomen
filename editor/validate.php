4d4ad0419a9eb4967d0224d1d98abe8c844c2e24
Michael Tolly <miketolly@gmail.com>
2014-08-13 20:15:43 -0500

    Cleaner validation code

--- editor/validate.php
+++ editor/validate.php
@@ -13,7 +13,7 @@ function scandir_real($dir) {
 }
 
 function canonical($str) {
-  return str_replace( ' ', '_', strtolower($str) );
+  return str_replace( ' ', '_', strtolower( trim($str) ) );
 }
 
 function validateDataset($dir) {
@@ -25,43 +25,72 @@ function validateDataset($dir) {
   // var_dump($species->data[0]['name']);
   // var_dump($species->titles[0]);
 
-  $species_images = scandir_real("$dir/species");
-  // var_dump($species_images[0]);
-
-  $feature_dirs = scandir_real("$dir/features");
-  $features = [];
-  foreach ($feature_dirs as $feature) {
-    $features[$feature] = [];
-    foreach (scandir_real("$dir/features/$feature") as $image) {
-      $features[$feature][$image] = true;
+  // Find all species images
+  $img_species = [];
+  foreach (scandir_real("$dir/species") as $img) {
+    if ( preg_match('/^(\w+)-[\w-]+\.(png|jpg)$/', $img, $matches) ) {
+      $img_species[ $matches[1] ] = true;
+    }
+    else {
+      $errors["Couldn't parse species image: $img"] = true;
     }
   }
-  // var_dump($features['stem_texture'][0]);
 
-  // Check that all features have images
+  // Find all species in the CSV
+  $csv_species = [];
+  foreach ($species->data as $spec) {
+    $csv_species[ canonical( $spec['name'] ) ] = true;
+  }
+
+  // Find all feature images
+  $feature_dirs = scandir_real("$dir/features");
+  $img_features = [];
+  foreach ($feature_dirs as $feature) {
+    $img_features[$feature] = [];
+    foreach (scandir_real("$dir/features/$feature") as $image) {
+      $ext = substr($image, -4);
+      if ($ext == '.png' || $ext == '.jpg') {
+        $img_features[$feature][substr($image, 0, -4)] = true;
+      }
+      else {
+        $errors["Feature image couldn't be parsed: $feature/$image"] = true;
+      }
+      $img_features[$feature][$image] = true;
+    }
+  }
+
+  // Find all features which are present in the species
+  $spec_features = [];
   foreach ($species->data as $spec) {
     foreach ($spec as $k => $vstr) {
       $k = canonical($k);
       if ($k == 'name' || $k == 'display_name' || $k == 'description') continue;
-      $values = array_map( 'trim', explode(',', $vstr) );
-      if ( array_key_exists($k, $features) ) {
-        foreach ($values as $value) {
-          $value = canonical($value);
-          if ( empty($value) ) continue;
-          if ( array_key_exists("$value.png", $features[$k])
-            || array_key_exists("$value.jpg", $features[$k]) ) {
-            // No problem here
-          }
-          else
-          {
-            $errors["Feature value doesn't have an image: $k -> $value"] = true;
-          }
-        }
+
+      if ( empty($spec_features[$k]) )
+      {
+        $spec_features[$k] = [];
       }
-      else {
-        $errors["Feature doesn't have a folder: $k"] = true;
+      $values = array_map( 'trim', explode(',', $vstr) );
+      foreach ($values as $v) {
+        $spec_features[$k][canonical($v)] = true;
       }
     }
+  }
+
+  // Validation: find feature images which aren't used by any species
+  // TODO
+
+  // Validation: find species features which don't have images
+  // TODO
+
+  // Validation: find species images which don't have CSV rows
+  foreach (array_diff_key($img_species, $csv_species) as $spec => $v) {
+    $errors["Species has image but no CSV row: $spec"] = true;
+  }
+
+  // Validation: find species rows which don't have images
+  foreach (array_diff_key($csv_species, $img_species) as $spec => $v) {
+    $errors["Species has CSV row but no image: $spec"] = true;
   }
 
   var_dump($errors);
