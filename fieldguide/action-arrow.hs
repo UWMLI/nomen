5804234486fb7c131184d5f31229b46533ae6f58
Michael Tolly <miketolly@gmail.com>
2014-10-02 12:01:36 -0500

    coffeescript preprocessor, may or may not use

---
+++ fieldguide/action-arrow.hs
@@ -0,0 +1,30 @@
+#!/usr/bin/env runhaskell
+module Main where
+
+import Data.Char (isSpace)
+
+indent :: String -> Int
+indent (' '  : s) = 1 + indent s
+indent ('\t' : s) = 2 + indent s
+indent s          = if all isSpace s then maxBound else 0
+
+dropLast :: [a] -> [a]
+dropLast (x : xs@(_ : _)) = x : dropLast xs
+dropLast _                = []
+
+main :: IO ()
+main = interact $ unlines . go [] . lines where
+
+  go, go' :: [Int] -> [String] -> [String]
+
+  -- First check if we have to add a "return" line
+  go _           []          = []
+  go ns@(n : nt) ls@(l : lt) = if indent l <= n
+    then (replicate (n + 2) ' ' ++ "return") : go nt ls
+    else go' ns ls
+  go [] ls = go' [] ls
+
+  -- Then check if this line starts an action
+  go' ns ls@(l : lt) = if reverse (take 3 $ reverse l) `elem` ["->>", "=>>"]
+    then dropLast l : go (indent l : ns) lt
+    else l : go ns lt
