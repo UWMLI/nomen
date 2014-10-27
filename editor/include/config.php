12288d4106f8ae3746da0a5ae09cfeda5ea420f8
Michael Tolly <miketolly@gmail.com>
2014-10-27 11:10:38 -0500

    Basic login system working

---
+++ editor/include/config.php
@@ -0,0 +1,12 @@
+<?php
+
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
+define("HOST", "127.0.0.1");
+define("USER", "root");
+define("PASSWORD", "root");
+define("DATABASE", "fieldguide");
+define("PORT", 8889);
+
+define("SECURE", FALSE);
