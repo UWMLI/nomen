c455ee7aab76ee8500046cf298908a9dcbe21fb8
Michael Tolly <miketolly@gmail.com>
2014-10-30 11:32:44 -0500

    Allow null author in api query

--- editor/www/api.php
+++ editor/www/api.php
@@ -6,7 +6,7 @@ header('Content-Type: application/json');
 
 if ($stmt = $mysqli->prepare("SELECT d.id, d.title, d.version, d.description, u.email
   FROM datasets AS d
-  INNER JOIN users AS u ON d.user_id = u.id")) {
+  LEFT JOIN users AS u ON d.user_id = u.id")) {
   $stmt->execute();
   $stmt->store_result();
 
