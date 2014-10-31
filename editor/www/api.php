7c639f6277c2ebcb9c7d5adab595d601d73e3173
Michael Tolly <miketolly@gmail.com>
2014-10-31 14:21:05 -0500

    API uses new db fns

--- editor/www/api.php
+++ editor/www/api.php
@@ -4,25 +4,18 @@ require_once '../include/db.php';
 
 header('Content-Type: application/json');
 
-if ($stmt = $mysqli->prepare("SELECT d.id, d.title, d.version, d.description, u.email
+$datasets = [];
+$rows = DB::query('SELECT d.id, d.title, d.version, d.description, u.email
   FROM datasets AS d
-  LEFT JOIN users AS u ON d.user_id = u.id")) {
-  $stmt->execute();
-  $stmt->store_result();
-
-  $datasets = [];
-
-  $stmt->bind_result($id, $title, $version, $description, $email);
-  while ($row = $stmt->fetch()) {
-    $datasets[] = [
-      'id' => DATASET_PREFIX . $id,
-      'title' => $title,
-      'version' => $version,
-      'description' => $description,
-      'author' => $email,
-      'url' => 'datasets/' . $id . '.zip',
-    ];
-  }
-
-  echo json_encode($datasets, JSON_PRETTY_PRINT);
+  LEFT JOIN users AS u ON d.user_id = u.id');
+foreach ($rows as $row) {
+  $datasets[] = [
+    'id' => DATASET_PREFIX . $row['id'],
+    'title' => $row['title'],
+    'version' => $row['version'],
+    'description' => $row['description'],
+    'author' => $row['email'],
+    'url' => 'datasets/' . $row['id'] . '.zip',
+  ];
 }
+echo json_encode($datasets, JSON_PRETTY_PRINT);
